8512386979c06c076b69860d6d128c844f2f50ae
曾涛 <so.zengtao@gmail.com>
2016-05-13 15:56:01 +0800

    Roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Rolify & Rolify Callbacks
+  rolify before_add: :before_add_method, after_add: :after_add_method
+
+  def before_add_method (role)
+    puts self.id.to_s + ' ' + role.name + ' is going to be add'
+  end
+
+  def after_add_method (role)
+    puts self.id.to_s + ' ' + role.name + ' was added'
+  end
 end
